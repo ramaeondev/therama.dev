2b85f386987c5a1092c20d4f7e9103368cc73fc4
ramaeon <Rama.eon@gmail.com>
2025-04-22 23:10:21 +0530

    Tailwind css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
+  mode: 'jit',
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    safelist: [
+      'dark:border-white',
+      'dark:hover:border-pink-500',
+      'dark:hover:text-pink-500',
+      'hover:text-lightBlue-600',
+      'hover:border-lightBlue-600'
+    ]
+  },
+  darkMode: 'class',
   theme: {
     extend: {},
     colors: {
